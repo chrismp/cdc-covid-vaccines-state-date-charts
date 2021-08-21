688040eaa52ca3122488d479fc449c4fd38a5500
Chris P <christopher.mark.persaud@gmail.com>
2021-08-21 15:26:12 -0400

    download files

---
+++ download-process.r
@@ -0,0 +1,28 @@
+library(dplyr)
+
+resourceID <- 'unsk-b7fc'
+source('download-cdc-api-data/download.r')
+
+rb$date_formatted <- as.Date(rb$date)
+
+fl <- filter(
+  .data = rb,
+  location == 'FL'
+)
+
+o <- 'output'
+dir.create(o)
+
+write.csv(
+  x = rb,
+  file = paste0(o,'/vaccines by state and date.csv'),
+  na = '',
+  row.names = F
+)
+
+write.csv(
+  x = fl,
+  file = paste0(o,'/fl vaccines by date.csv'),
+  na = '',
+  row.names = F
+)
