31933300d6084c9f6e77856b0ee41109d0590e05
Chris P <christopher.mark.persaud@gmail.com>
2021-08-21 16:17:16 -0400

    add daily admin vax change

--- download-process.r
+++ download-process.r
@@ -1,4 +1,8 @@
+# https://data.cdc.gov/Vaccinations/COVID-19-Vaccinations-in-the-United-States-Jurisdi/unsk-b7fc
+
 library(dplyr)
+
+func.SubtractFromPrev <- function(x){return(x - lag(x))}
 
 resourceID <- 'unsk-b7fc'
 source('download-cdc-api-data/download.r')
@@ -10,12 +14,50 @@ fl <- filter(
   location == 'FL'
 )
 
+fl <- fl[order(date_formatted),]
+fl$administered <- as.numeric(fl$administered)
+fl$new_vaccines_administered <- func.SubtractFromPrev(fl$administered)
+
+# fl$new_case_7day_avg <- mvavg(
+#   x = fl$new_case,
+#   n = 7
+# )
+# 
+# fl$new_deaths_7day_avg <- mvavg(
+#   x = fl$new_death,
+#   n = 7
+# )
+
+flslim <- fl[,c(
+  'date',
+  'date_formatted',
+  'administered',
+  'administered_dose1_recip',
+  'administered_dose1_pop_pct',
+  'administered_dose1_recip_1',
+  'administered_dose1_recip_2',
+  'administered_dose1_recip_3',
+  'administered_dose1_recip_4',
+  'administered_dose1_recip_5',
+  'administered_dose1_recip_6',
+  'series_complete_yes',
+  'series_complete_pop_pct',
+  'series_complete_12plus',
+  'series_complete_12pluspop',
+  'series_complete_18plus',
+  'series_complete_18pluspop',
+  'series_complete_65plus',
+  'series_complete_65pluspop',
+  'new_vaccines_administered'
+)]
+
+
 o <- 'output'
 dir.create(o)
 
 write.csv(
   x = rb,
-  file = paste0(o,'/vaccines-by-state-an-date.csv'),
+  file = paste0(o,'/vaccines-by-state-and-date.csv'),
   na = '',
   row.names = F
 )
@@ -23,6 +65,13 @@ write.csv(
 write.csv(
   x = fl,
   file = paste0(o,'/fl-vaccines-by-date.csv'),
+  na = '',
+  row.names = F
+)
+
+write.csv(
+  x = flslim,
+  file = paste0(o,'/fl-slim-vaccines-by-date.csv'),
   na = '',
   row.names = F
 )
